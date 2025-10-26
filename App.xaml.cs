4688952f5c1e5a4866452e13f82bd974e8a89487
ytz <1472290813@qq.com>
2025-10-26 23:26:48 +0800

    解决了启动报错的问题

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,13 @@ namespace CyberNote
     /// </summary>
     public partial class App : Application
     {
+        // App.xaml.cs
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var window = new MainWindow();
+            window.Show();
+        }
     }
 
 }
